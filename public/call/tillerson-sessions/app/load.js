f87d605d129b46f38ce3ce7b608a125114b049bc
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-31 16:39:26 -0700

    More copy editing

--- public/call/tillerson-sessions/app/load.js
+++ public/call/tillerson-sessions/app/load.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('a#terms_link').click(function(){
-		alert("Advocacy Commons SMS Terms & Conditions:\nBy signing up and entering my telephone number, I consent to receive and authorize Advocacy Commons to send (i) SMS text messages, (ii) prerecorded audio messages (including calls to cellular phones), or (iii) other communications sent by an autodialer to my mobile device.");
+		alert("Advocacy Commons SMS Terms & Conditions:\nBy signing up and entering my telephone number, I consent to receive and authorize Advocacy Commons and its partner organizations to send (i) SMS text messages, (ii) prerecorded audio messages (including calls to cellular phones), or (iii) other communications sent by an autodialer to my mobile device.");
 	});
 	$('a#reveal_email_subscription_preferences').click(function(){
 	  $('div#email_subscription_preferences').slideToggle();
