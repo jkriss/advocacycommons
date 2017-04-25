68b2e990168134bf63a3c9aaa33822b4d48ed26d
rabble <evan@protest.net>
2017-04-25 10:00:43 -0700

    fixing typo in the link format

--- client/app/bundles/Events/components/Event.jsx
+++ client/app/bundles/Events/components/Event.jsx
@@ -1,6 +1,5 @@
 import moment from 'moment';
 import React, { PropTypes } from 'react';
-var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
 
 export default class Event extends React.Component {
@@ -25,9 +24,8 @@ export default class Event extends React.Component {
       <td>{this.state['invited-count']}</td>
       <td>{this.state['rsvp-count']}</td>
       <td>{this.state['attended-count']}</td>
-      <td>
-          <Link className='event_list-toggle' to='/events/' + {this.id}>Battle</Link>/td>
-    </tr>
+      <td><Link className='event_list-toggle' to={`/events/${this.id}`}>See Attendees</Link></td>
+      </tr>
     );
   }
 }
